4e1264ca28d6ca1fbdd2ccb246d91ca6f79420ac
Haitham abu lamdi <hayhtam14@gmail.com>
2022-06-23 00:19:45 +0300

    trying to let js work

--- resources/views/includes/appJS.blade.php
+++ resources/views/includes/appJS.blade.php
@@ -1,5 +1,5 @@
   <!-- BEGIN VENDOR JS-->
-  <script src="{{ asset('app-assets/vendors/js/vendors.min.js') }}" type="text/javascript"></script>
+{{--  <script src="{{ asset('app-assets/vendors/js/vendors.min.js') }}" type="text/javascript"></script>--}}
   <!-- BEGIN VENDOR JS-->
   <!-- BEGIN PAGE VENDOR JS-->
   {{-- <script src="{{ asset('app-assets/vendors/js/charts/echarts/echarts.js') }}" type="text/javascript"></script>//
@@ -12,12 +12,12 @@
     type="text/javascript">
       //
   </script> --}}
-  <script src="{{ asset('app-assets/vendors/js/forms/icheck/icheck.min.js') }}" type="text/javascript"></script>
+{{--  <script src="{{ asset('app-assets/vendors/js/forms/icheck/icheck.min.js') }}" type="text/javascript"></script>--}}
   <!-- END PAGE VENDOR JS-->
   <!-- BEGIN MODERN JS-->
-  <script src="{{ asset('app-assets/js/core/app-menu.js') }}" type="text/javascript"></script>
-  <script src="{{ asset('app-assets/js/core/app.js') }}" type="text/javascript"></script>
-  <script src="{{ asset('app-assets/js/scripts/customizer.js') }}" type="text/javascript"></script>
+{{--  <script src="{{ asset('app-assets/js/core/app-menu.js') }}" type="text/javascript"></script>--}}
+{{--  <script src="{{ asset('app-assets/js/core/app.js') }}" type="text/javascript"></script>--}}
+{{--  <script src="{{ asset('app-assets/js/scripts/customizer.js') }}" type="text/javascript"></script>--}}
   <!-- END MODERN JS-->
   <!-- BEGIN PAGE LEVEL JS-->
   {{-- <script src="{{ asset('app-assets/js/scripts/pages/project-summary-task.js') }}" type="text/javascript"></script>//
@@ -25,8 +25,8 @@
   <script src="{{ asset('app-assets/js/scripts/pages/users-contacts.js') }}" type="text/javascript"></script> --}}
   <!-- END PAGE LEVEL JS-->
   {{-- for toastr --}}
-  <script src="{{ asset('app-assets/vendors/js/extensions/toastr.min.js') }}" type="text/javascript"></script>
-  <script src="{{ asset('app-assets/js/scripts/extensions/toastr.js') }}" type="text/javascript"></script>
+{{--  <script src="{{ asset('app-assets/vendors/js/extensions/toastr.min.js') }}" type="text/javascript"></script>--}}
+{{--  <script src="{{ asset('app-assets/js/scripts/extensions/toastr.js') }}" type="text/javascript"></script>--}}
 
   {{-- secure_asset --}}
   <script src="{{ secure_asset('app-assets/vendors/js/vendors.min.js') }}" type="text/javascript"></script>
